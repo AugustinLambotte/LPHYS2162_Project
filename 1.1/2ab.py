90832dae48361e4acb884be48bd3a349d382c1af
AugustinLambotte <augustin.lambotte@gmail.com>
2022-12-22 18:01:37 +0100

    Harmonizing code 2ab and plotin 2ab

--- 1.1/2ab.py
+++ 1.1/2ab.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 	in tree case diffentiating each other by tree different lambda values.
 	- C is the concentration of CO_2 in ppm
 """
-
+###################### - Simulation Parameter - ###################
 T_0 = 287
 C_0 = 270
 
@@ -19,17 +19,29 @@ lambda_1 = -0.79
 lambda_2 = -1.17
 lambda_3 = -1.78
 
-def F(CO_2_multiplication):
-	return np.log2(CO_2_multiplication)*3.71
-
-def temperature_increase(F, alpha, lamb):
-	return (-lamb - np.sqrt(lamb**2 - 4 * alpha * F))/(2 * alpha)
+CO2_1 = 2
+CO2_2 = 4
 
 alpha_min = -0.1
 alpha_max = 0.1
-iteration = 200
-detla_alpha = (alpha_max - alpha_min)/iteration
-alpha = np.arange(alpha_min, alpha_max, detla_alpha)
+detla_alpha = 0.00001
+alpha_distribution = np.arange(alpha_min, alpha_max, detla_alpha)
+
+###################### - Function definitions - ###########################
+
+
+def temperature_increase(CO_2_multiplication, alpha, lamb):
+	"""
+		Give the increase of temperature for a given perturbation in CO_2 concetration and given values of the feedback
+	"""
+	F = np.log2(CO_2_multiplication)*3.71
+	if alpha == 0:
+		return -F/lamb
+	else:
+		return (-lamb - np.sqrt(lamb**2 - 4 * alpha * F))/(2*alpha)
+
+
+############################ - Simulation - ########################## 
 #These are for a doubling of CO_2
 T_11 = []
 T_12 = []
@@ -38,33 +50,39 @@ T_13 = []
 T_21 = []
 T_22 = []
 T_23 = []
-for i in range(iteration):
-	T_11.append(temperature_increase(F(2),alpha[i],lambda_1))
-	T_12.append(temperature_increase(F(2),alpha[i],lambda_2))
-	T_13.append(temperature_increase(F(2),alpha[i],lambda_3))
 
-	T_21.append(temperature_increase(F(4),alpha[i],lambda_1))
-	T_22.append(temperature_increase(F(4),alpha[i],lambda_2))
-	T_23.append(temperature_increase(F(4),alpha[i],lambda_3))
+for alpha in alpha_distribution:
+	T_11.append(temperature_increase(CO2_1, alpha, lambda_1))
+	T_12.append(temperature_increase(CO2_1, alpha, lambda_2))
+	T_13.append(temperature_increase(CO2_1, alpha, lambda_3))
+
+	T_21.append(temperature_increase(CO2_2, alpha, lambda_1))
+	T_22.append(temperature_increase(CO2_2, alpha, lambda_2))
+	T_23.append(temperature_increase(CO2_2, alpha, lambda_3))
+
+
+##################### - Ploting the results - ####################
 
 plt.subplot(1,2,1)
-plt.plot(alpha,T_11, label = 'lambda = {}'.format(lambda_1))
-plt.plot(alpha,T_12, label = 'lambda = {}'.format(lambda_2))
-plt.plot(alpha,T_13, label = 'lambda = {}'.format(lambda_3))
-plt.ylabel('delta_T')
-plt.xlabel('alpha')
+plt.plot(alpha_distribution, T_11, label = 'lambda = {}'.format(lambda_1))
+plt.plot(alpha_distribution, T_12, label = 'lambda = {}'.format(lambda_2))
+plt.plot(alpha_distribution, T_13, label = 'lambda = {}'.format(lambda_3))
+plt.ylabel('delta_T (°K)')
+plt.xlabel('alpha (W/m^2 / K^2)')
 plt.title('2x CO_2 concentration')
 plt.xlim(-0.1,0.1)
+plt.grid()
 plt.legend()
 
 plt.subplot(1,2,2)
-plt.plot(alpha,T_21, label = 'lambda = {}'.format(lambda_1))
-plt.plot(alpha,T_22, label = 'lambda = {}'.format(lambda_2))
-plt.plot(alpha,T_23, label = 'lambda = {}'.format(lambda_3))
-plt.ylabel('delta_T')
-plt.xlabel('alpha')
+plt.plot(alpha_distribution, T_21, label = 'lambda = {}'.format(lambda_1))
+plt.plot(alpha_distribution, T_22, label = 'lambda = {}'.format(lambda_2))
+plt.plot(alpha_distribution, T_23, label = 'lambda = {}'.format(lambda_3))
+plt.ylabel('delta_T (°K)')
+plt.xlabel('alpha (W/m^2 /K^2)')
 plt.title('4x CO_2 concentration')
 plt.xlim(-0.1,0.1)
+plt.grid()
 plt.legend()
 
 
