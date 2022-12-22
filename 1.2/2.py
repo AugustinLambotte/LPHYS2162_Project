d37599d8dfad609a833eceef1f445822c0e1a694
AugustinLambotte <augustin.lambotte@gmail.com>
2022-12-22 10:39:00 +0100

    premier test question 1.2

--- 1.2/2.py
+++ 1.2/2.py
@@ -2,4 +2,105 @@ import numpy as np
 import random as rand
 import math
 import matplotlib.pyplot as plt
+"""
+    Here, the goal is to investigate the role of the thermal inertia C and the transient behaviour of the model.
+    A numerical scheme is implemented to solve (eq 3.). We will perform several times series, all of theme during 200y,
+    for all the value of alpha lambda and F used in the previous exercises.
+"""
+############ - Simulation Parameters - ############
+C = 8.36 * 10 ** 8 
 
+#Graph 1a. Same lambda, varying alpha
+lamb_1a = -0.88
+alpha_1a_1 = -0.035
+alpha_1a_2 = 0.03
+alpha_1a_3 = 0.058
+CO2_1a = 2
+
+#Graphs 1d. Same lambda, varying from linear to alpha = 0.058 and from one to two doubling of CO2
+lamb_1d = -1.28
+alpha_1d_1 = 0
+CO2_1d_1 = 2
+
+alpha_1d_2 = 0.058
+CO2_1d_2 = 2
+
+alpha_1d_3 = 0
+CO2_1d_3 = 4
+
+alpha_1d_4 = 0.058
+CO2_1d_4 = 4
+
+#Graph 2a and 2b.
+lamb_2ab_1 = -0.79
+lamb_2ab_2 = -1.17
+lamb_2ab_3 = -1.78
+
+########## - Temporal discretisation  - ###########
+delta_t_in_year = 0.5
+integration_time_in_year = 200 
+delta_t = delta_t_in_year * 365 * 24 * 60 * 60 # Using SI units
+integration_time = integration_time_in_year * 365 * 24 * 60 * 60 # Using SI units
+
+temporal_discretization = np.arange(0, integration_time, delta_t)
+
+############ - Function definition - ##############
+
+def delta_T_next_time_step(CO2_multiplication, lamb, alpha, delta_T):
+    # return the next time step with Euler Forward method.
+    F = np.log2(CO2_multiplication)*3.71
+    return (delta_t/C) * (F + lamb * delta_T + alpha * delta_T**2) + delta_T
+
+################# - Simulation - ###################
+T_1a_1 = []
+T_1a_2 = []
+T_1a_3 = []
+
+T_1d_1 = []
+T_1d_2 = []
+T_1d_3 = []
+T_1d_4 = []
+for t in temporal_discretization:
+    if t == 0:
+        T_1a_1.append(0)
+        T_1a_2.append(0)
+        T_1a_3.append(0)
+
+        T_1d_1.append(0)
+        T_1d_2.append(0)
+        T_1d_3.append(0)
+        T_1d_4.append(0)
+    else:
+        T_1a_1.append(delta_T_next_time_step(CO2_1a, lamb_1a, alpha_1a_1, T_1a_1[-1]))
+        T_1a_2.append(delta_T_next_time_step(CO2_1a, lamb_1a, alpha_1a_2, T_1a_2[-1]))
+        T_1a_3.append(delta_T_next_time_step(CO2_1a, lamb_1a, alpha_1a_3, T_1a_3[-1]))
+
+        T_1d_1.append(delta_T_next_time_step(CO2_1d_1, lamb_1d, alpha_1d_1, T_1d_1[-1]))
+        T_1d_2.append(delta_T_next_time_step(CO2_1d_2, lamb_1d, alpha_1d_2, T_1d_2[-1]))
+        T_1d_3.append(delta_T_next_time_step(CO2_1d_3, lamb_1d, alpha_1d_3, T_1d_3[-1]))
+        T_1d_4.append(delta_T_next_time_step(CO2_1d_4, lamb_1d, alpha_1d_4, T_1d_4[-1]))
+
+# We creat 3 subplots, first for value param from fig 1a, second from fig 1d and last for fig 2.
+plt.subplot(1,3,1)
+plt.plot(temporal_discretization,T_1a_1, label = '1a.1')
+plt.plot(temporal_discretization,T_1a_2, label = '1a.2')
+plt.plot(temporal_discretization,T_1a_3, label = '1a.3')
+plt.xlabel('temps (s)')
+plt.ylabel('delta_T (°K)')
+plt.legend()
+plt.title('thermal inertia, parameter value from fig 1a.')
+
+plt.subplot(1,3,2)
+plt.plot(temporal_discretization,T_1d_1, label = '1d.1')
+plt.plot(temporal_discretization,T_1d_2, label = '1d.2')
+plt.plot(temporal_discretization,T_1d_3, label = '1d.3')
+plt.plot(temporal_discretization,T_1d_4, label = '1d.4')
+plt.legend()
+plt.xlabel('temps (s)')
+plt.ylabel('delta_T (°K)')
+plt.title('thermal inertia, parameter value from fig 1d.')
+
+
+plt.subplot(1,3,3)
+
+plt.show()
