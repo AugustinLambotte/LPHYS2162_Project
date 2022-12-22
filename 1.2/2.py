40694628a9ce1277d7167d6e8bf33df70e2610c6
AugustinLambotte <augustin.lambotte@gmail.com>
2022-12-22 11:03:43 +0100

    2a and 2b resolved

--- 1.2/2.py
+++ 1.2/2.py
@@ -36,12 +36,13 @@ lamb_2ab_1 = -0.79
 lamb_2ab_2 = -1.17
 lamb_2ab_3 = -1.78
 
-alpha_2ab = 0.3
+alpha_2ab = 0.03
 
 CO2_2a = 2
 CO2_2b = 4
 
 ########## - Temporal discretisation  - ###########
+
 delta_t_in_year = 0.5
 integration_time_in_year = 200 
 delta_t = delta_t_in_year * 365 * 24 * 60 * 60 # Using SI units
@@ -102,8 +103,8 @@ for t in temporal_discretization:
         T_1d_4.append(delta_T_next_time_step(CO2_1d_4, lamb_1d, alpha_1d_4, T_1d_4[-1]))
 
         T_2a_1.append(delta_T_next_time_step(CO2_2a, lamb_2ab_1, alpha_2ab, T_2a_1[-1]))
-        T_2a_2.append(delta_T_next_time_step(CO2_2a, lamb_2ab_2, alpha_2ab, T_2a_3[-1]))
-        T_2a_3.append(delta_T_next_time_step(CO2_2a, lamb_2ab_3, alpha_2ab, T_2a_2[-1]))
+        T_2a_2.append(delta_T_next_time_step(CO2_2a, lamb_2ab_2, alpha_2ab, T_2a_2[-1]))
+        T_2a_3.append(delta_T_next_time_step(CO2_2a, lamb_2ab_3, alpha_2ab, T_2a_3[-1]))
 
         T_2b_1.append(delta_T_next_time_step(CO2_2b, lamb_2ab_1, alpha_2ab, T_2b_1[-1]))
         T_2b_2.append(delta_T_next_time_step(CO2_2b, lamb_2ab_2, alpha_2ab, T_2b_2[-1]))
